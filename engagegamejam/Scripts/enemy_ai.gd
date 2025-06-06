85f619edcb397a0210c126ab9f616a9464be3f47
Rados Radovanovic <radosradovanovic@gmail.com>
2025-06-06 22:09:42 +0200

    funckiaj napada

--- engagegamejam/Scripts/enemy_ai.gd
+++ engagegamejam/Scripts/enemy_ai.gd
@@ -1,6 +1,9 @@
 extends CharacterBody2D
 @onready var target = $"../Main_Character"
 var speed = 100
+var attack_range = 20.0
+var attack_cooldown = 1.0 # u sekundama
+var can_attack = true
 
 
 func _physics_process(_delta: float) -> void:
@@ -12,9 +15,24 @@ func _physics_process(_delta: float) -> void:
 	move_and_slide()
 
 
-# # Called when the node enters the scene tree for the first time.
-# func _ready() -> void:
-# 	pass # Replace with function body.
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var timer = Timer.new()
+	timer.one_shot = true
+	timer.wait_time = attack_cooldown
+	timer.name = "AttackCooldownTimer"
+	add_child(timer)
+	timer.connect("timeout", Callable(self, "_on_attack_cooldown_timeout"))
+
+
+func attack():
+	can_attack = false
+	print("Napad!")  # Zamijeni kasnije sa nanosenjem stete
+	$AttackCooldownTimer.start()
+
+
+func _on_attack_cooldown_timeout():
+	can_attack = true
 
 
 # # Called every frame. 'delta' is the elapsed time since the previous frame.
