9d5fdaa253424c1a9a1829f7adac317ce7acf64f
Rados Radovanovic <radosradovanovic@gmail.com>
2025-06-07 02:07:55 +0200

    izbjegavanje zidova od starne AI-a

--- engagegamejam/Scripts/enemy_ai.gd
+++ engagegamejam/Scripts/enemy_ai.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
-@onready var target = $"../Main_Character"
-const speed = 100
+@onready var target = $"../../Main_Character"
+@onready var agent = $NavigationAgent2D
+const speed = 50
 const attack_cooldown = 1.0 # u sekundama
 const damage = 10
 var player_in_area = false
@@ -15,11 +16,24 @@ func _ready():
 	
 	$AttackArea.body_entered.connect(_on_attack_area_body_entered)
 	$AttackArea.body_exited.connect(_on_attack_area_body_exited)
+	
+	agent.target_position = target.global_position
 
-func _physics_process(_delta: float) -> void:
-	var direction = (target.position - position).normalized()
-	velocity = direction * speed
-	look_at(target.position)
+func _physics_process(delta: float) -> void:
+	agent.target_position = target.global_position
+
+	if agent.is_navigation_finished():
+		velocity = Vector2.ZERO
+	else:
+		var next_path_point = agent.get_next_path_position()
+		var distance = global_position.distance_to(next_path_point)
+		if distance > 2.0: # Only move if not already at the point
+			var direction = (next_path_point - global_position).normalized()
+			velocity = direction * speed
+			look_at(next_path_point)
+		else:
+			velocity = Vector2.ZERO
+
 	move_and_slide()
 
 func _on_attack_area_body_entered(body):
