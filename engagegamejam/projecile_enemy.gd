461d55c27973add9937c16d5240ca164b45fb598
Rados Radovanovic <radosradovanovic@gmail.com>
2025-06-07 21:58:45 +0200

    ai enemy koji automatmski puca

--- engagegamejam/projecile_enemy.gd
+++ engagegamejam/projecile_enemy.gd
@@ -1,16 +1,23 @@
 extends Node2D
-var bullet_path = preload("res://character_body_2d.tscn")
+var bullet_path = preload("res://projektil.tscn")
 
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	look_at(get_global_mouse_position())
-	if Input.is_action_just_pressed("shoot"):
-		fire()
+
+
+func _ready():
+	var timer = Timer.new()
+	timer.wait_time = 1.0
+	timer.one_shot = false
+	timer.autostart = true
+	add_child(timer)
+	timer.connect("timeout", Callable(self, "fire"))
 
 
 func fire():
 	var bullet = bullet_path.instantiate()
 	bullet.direction = rotation
-	bullet.pos = $Node2D.global_position
+	bullet.pos = $CharacterBody2D/Node2D.global_position
 	bullet.rota = global_rotation
 	get_parent().add_child(bullet)
